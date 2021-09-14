735cde7a59cfb7c5e570b34ad15edf2696a24f7c
Ealendra Yadav <Ealendra Yadav@DESKTOP-5J9SM0S>
2021-09-14 00:11:39 -0700

    WEB-853 FIRST TIME COMMIT THE CODE

---
+++ src/main/java/in/hca/babu/MessageRunner.java
@@ -0,0 +1,15 @@
+package in.hca.babu;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("welcome to GIT HUB");
+
+	}
+
+}
